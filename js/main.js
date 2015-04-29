57160c02ea766a99094794aba6d98fbefbc3736a
wendy <wendycaner@gmail.com>
2015-04-29 15:04:10 +0800

    Modified sub nav

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $.get('/public/data.json').done(function (data) {
     data.forEach(function(t, index){
-      var html = _.template($('#t-table').html())({category: t.category.replace(' ', '-'),table_index: index});
+      var html = _.template($('#t-table').html())({category: t.category.replace(' ', '-').replace('\n',''),table_index: index});
       $('#container').append(html);
       t.data.forEach(function(item, i){
         var tr = _.template($('#t-tr').html())({text: item,index: i + 1});
